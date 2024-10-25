719e6e7c6277ca9fe5277c0d15ce4591ca0ecc41
suresh <sureshkumarbe04@gmail.com>
2024-10-25 18:49:41 +0530

    Connect mongoDB Collection

--- App.tsx
+++ App.tsx
@@ -5,25 +5,26 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Button,
+  FlatList,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+// import Meteor, {Mongo, withTracker} from '@meteorrn/core';
+import Meteor from 'react-native-meteor';
+// import NetInfo from '@react-native-community/netinfo';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -56,7 +57,156 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 }
 
 function App(): React.JSX.Element {
+  const collectionName = ['links', 'users'],
+    currentCollectionName = collectionName?.[0];
+  const IPAddress = '192.168.1.250';
+  const baseURL = `http://${IPAddress}:3000/api`;
   const isDarkMode = useColorScheme() === 'dark';
+  const [insertedData, setInsertedData] = useState<any[]>([]);
+  const [userData, setUserData] = useState<any>('');
+  const [deleteId, setDeleteId] = useState('');
+
+  // const connectMongo = () => {
+  //   try {
+  //     Meteor.connect('localhost:27017/websocket');
+  //     const Todos = new Mongo.Collection('suresh');
+  //   } catch (error: any) {
+  //     console.log('catch: ', error?.message);
+  //   }
+  // };
+
+  // Insert data using Meteor func
+  const insertData = async () => {
+    try {
+      Meteor.call(
+        `${currentCollectionName}.insert`,
+        {name: userData},
+        (err: any, res: any) => {
+          if (err) {
+            console.log('Insert err: ', err);
+          } else {
+            console.log('res: ', res);
+            setUserData('');
+            setTimeout(() => {
+              getData();
+            }, 2000);
+          }
+        },
+      );
+    } catch (error: any) {
+      console.log('catch insertData: ', error?.message);
+    }
+  };
+
+  // Insert data using API
+  const insertDataApi = async () => {
+    try {
+      // System IP Address
+      const res = await fetch(`/links`, {
+        method: 'post',
+        body: JSON.stringify({name: userData}),
+      });
+      console.log('insertDataApi: ', await res.json());
+      getData();
+    } catch (error: any) {
+      console.log('catch insertDataApi: ', error?.message);
+    }
+  };
+
+  // Delete data using API
+  const deleteDataApi = async (delId: string) => {
+    try {
+      const res = await fetch(`${baseURL}/${delId}`, {
+        method: 'delete',
+        body: JSON.stringify({name: userData}),
+      });
+      setUserData('');
+      console.log('insertDataApi: ', await res.json());
+      getData();
+    } catch (error: any) {
+      console.log('catch insertDataApi: ', error?.message);
+    }
+  };
+
+  const deleteData = async (delId: string) => {
+    try {
+      // const res = await Meteor.collection(currentCollectionName).insertAsync({
+      //   name: userData,
+      // });
+      const res = await Meteor.collection(currentCollectionName).remove({
+        _id: delId,
+      }); // This directly manipulates the collection
+      console.log('deleteData: ', res);
+      getData();
+    } catch (error: any) {
+      console.log('catch deleteData: ', error?.message);
+    }
+  };
+
+  // Get data using API
+  const getDataApi = async () => {
+    try {
+      const res = await fetch(`${baseURL}/links`, {
+        method: 'get',
+      });
+      // console.log('getDataApi: ', await res.json());
+    } catch (error: any) {
+      console.log('catch getDataApi: ', error?.message);
+    }
+  };
+
+  // @meteorrn/core using this package unable retrieve data from Collection
+  const connectMongo = async () => {
+    try {
+      Meteor.connect('ws://192.168.1.250:3000/websocket');
+      // if (getColletion.ready()) {
+      const data = await Meteor.collection('links').find();
+      console.log('data: ', data);
+      // }
+    } catch (error: any) {
+      console.log('catch connectMongo: ', error?.message);
+    }
+  };
+
+  // Get data using Meteor func
+  const getData = async () => {
+    try {
+      const todosData = Meteor.collection(currentCollectionName).find();
+      if (todosData) {
+        setInsertedData(todosData);
+      }
+    } catch (error: any) {
+      console.log('catch getData: ', error?.message);
+    }
+  };
+
+  useEffect(() => {
+    getDataApi();
+  }, []);
+
+  useEffect(() => {
+    // connectMongo();
+    Meteor.connect(`ws://${IPAddress}:3000/websocket`);
+
+    // Subscribe to 'todos' collection
+    const handle = Meteor.subscribe(currentCollectionName);
+
+    // Check for subscription readiness and fetch data
+    const interval = setInterval(() => {
+      if (handle.ready()) {
+        getData();
+        clearInterval(interval);
+      } else {
+        console.log('Else: ', handle.ready());
+      }
+    }, 500);
+
+    // Clean up on unmount
+    return () => {
+      handle.stop();
+      clearInterval(interval);
+    };
+  }, []);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -75,21 +225,48 @@ function App(): React.JSX.Element {
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            padding: 15,
+            rowGap: 15,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <View>
+            <TextInput
+              placeholder="Enter text"
+              value={userData}
+              onChangeText={t => setUserData(t)}
+              style={{borderWidth: 0.4, padding: 12}}
+              onSubmitEditing={() => {
+                console.log('Call');
+                String(userData)?.trim() && insertData();
+              }}
+            />
+          </View>
+          <Button title="Clear input" onPress={() => setUserData('')} />
+          <FlatList
+            data={insertedData}
+            keyExtractor={(item, index) => String(index)}
+            renderItem={({item, index}) => (
+              <TouchableOpacity
+                style={{padding: 10, flexDirection: 'row', columnGap: 12}}
+                onPress={() => {
+                  deleteDataApi(item?._id);
+                }}
+                onLongPress={() => {
+                  deleteData(item?._id);
+                }}>
+                <Text>{index + 1}</Text>
+                <Text>{item?.name}</Text>
+              </TouchableOpacity>
+            )}
+          />
+          <Button
+            title="Insert data using Api"
+            onPress={() => insertDataApi()}
+          />
+          {/* <Button
+            title="Delete data using Api"
+            onPress={() => deleteDataApi()}
+          /> */}
+          <Text>Click on any row it will delete directly from collection</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
